ab864bcf7b6b559c94f3b3818af01d663cfa9a2c
miners <bionetworkslive@gmail.com>
2014-06-04 01:47:15 -0400

    updates2

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -24,7 +24,7 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of // TODO: needs to adjusted for checkpoint checks, also see main.cpp
-        (         0, uint256("0x09ebd987d0d85433320f7fa22a11fe5b93639686dd89e28f3181ae173740f1fb"))
+        (         1, uint256("0x09ebd987d0d85433320f7fa22a11fe5b93639686dd89e28f3181ae173740f1fb"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
